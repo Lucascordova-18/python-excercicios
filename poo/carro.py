024fede660ec6d777a62e07de5ede70af15daa1a
Lucas Cordova <lucascordovac@icloud.com>
2026-03-09 23:56:23 -0300

    Atualizando exercicio de Carro

--- poo/carro.py
+++ poo/carro.py
@@ -3,26 +3,21 @@ class Carro():
     def __init__(self, car):
         self.car = car
         self.ligar = False
-        self.desligar = True
         self.acelerador = False
-        self.breque = False
         self.velocidade = 0
 
     def partida (self):
-
-        if not self.ligar and self.desligar:
+        if not self.ligar:
             print(f"O carro {self.car} acabou de ligar")
             self.ligar = True
-            self.desligar = False
 
-        elif self.ligar and not self.desligar:
+        elif self.ligar:
             print ("O carro ja está ligado!")
 
     def desliga(self):
-        if self.ligar and not self.desligar:
+        if self.ligar:
             print(f"O carro {self.car} acabou de ser desligado!")
             self.ligar = False
-            self.desligar = True
         
         else:
             print(f"O carro {self.car} ja foi desligado")
@@ -30,21 +25,34 @@ class Carro():
     def acelerar(self):
         if self.ligar:
             print("Seu carro esta acelerando")
+            self.velocidade += 10
+            print(f"{self.velocidade}Km/h")
         else:
             print(f"O seu {self.car} precisa estar ligado para funcionar")
 
     def freiar(self):
         if self.ligar:
-            if self.velocidade == 0:
-                print("Seu carro ja esta fereiado!")
-
+            if self.velocidade >= 0:
+                print("Seu carro ja esta fereiando!")
+                self.velocidade -= 10
+                print(f"{self.velocidade}Km/h")
+            else:
+                print(f"O carro {self.car} esta parado!")
         else:
             print(f"O seu {self.car} precisa estar ligado para funcionar")
 
-bmw = Carro("BMW 320i")
-bmw.partida()
-bmw.acelerar()
-bmw.partida()
-bmw.desliga()
-bmw.acelerar()
-bmw.desliga()
+
+car = Carro(input("Input your car: "))
+while True:
+
+    acao = input("O que deseja fazer? (partida, acelerar, freiar, desligar): ")
+    if acao == "partida":
+        car.partida()
+    elif acao == "acelerar":
+        car.acelerar()
+    elif acao == "freiar":
+        car.freiar()
+    elif acao == "desligar":
+        car.desliga()
+    else:
+        print("Ação inválida. Tente novamente.")
